793f31cb7a6b3cfc76d265c1dd99077db0107cc1
AdamGraser <adamgraser@interia.pl>
2015-01-12 00:12:24 +0100

    Dodanie modelu Registrar\DailyVisitsListItem

---
+++ MedicalCenter.Models/Registrar/DailyVisitsListItem.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCenter.Models.Registrar
+{
+    /// <summary>
+    /// Reprezentuje jedną pozycję z listy wizyt z danego dnia w widoku Poradnia - Lekarz rejestrowania wizyty przez rejestratorkę.
+    /// </summary>
+    class DailyVisitsListItem
+    {
+        // Public getters, private setters
+        #region Public properties
+
+        /// <summary>
+        /// Przechowuje datę i czas rozpoczęcia wizyty.
+        /// Jest to wartość z kolumny DateOfVisit z tabeli M_Visits.
+        /// </summary>
+        public DateTime DateOfVisit { get; private set; }
+
+        /// <summary>
+        /// Przechowuje nazwisko pacjenta, dla którego ta wizyta została zarejestrowana.
+        /// Jest to wartość z kolumny LastName z tabeli M_Patients.
+        /// </summary>
+        public string PatientLastName { get; private set; }
+
+        /// <summary>
+        /// Przechowuje imię pacjenta, dla którego ta wizyta została zarejestrowana.
+        /// Jest to wartość z kolumny FirstName z tabeli M_Patients.
+        /// </summary>
+        public string PatientFirstName { get; private set; }
+
+        /// <summary>
+        /// Określa czy wizyta została zarejestrowana jako nagły przypadek.
+        /// Jest to wartość z kolumny IsEmergency z tabeli M_Visits.
+        /// </summary>
+        public bool IsEmergency { get; private set; }
+
+        #endregion // Public properties
+
+        #region Ctors
+
+        /// <summary>
+        /// Ukrycie domyślnego konstruktora.
+        /// </summary>
+        private DailyVisitsListItem() { }
+
+        /// <summary>
+        /// Tworzy obiekt listy wizyt, reprezentujący "wolną godzinę" (na którą nikt dotychczas nie zarejestrował wizyty do wskazanego lekarza).
+        /// </summary>
+        /// <param name="DateOfVisit">Data i godzina, na którą wciąż można zarejestrować wizytę.</param>
+        public DailyVisitsListItem(DateTime DateOfVisit)
+        {
+            this.DateOfVisit = DateOfVisit;
+            this.IsEmergency = false;
+        }
+
+        /// <summary>
+        /// Tworzy obiekt listy wizyt, reprezentujący zarejestrowaną wizytę.
+        /// </summary>
+        /// <param name="DateOfVisit">Data i godzina, na którą dana wizyta została zarejestrowana.</param>
+        /// <param name="PatientLastName">Nazwisko pacjenta, dla którego dana wizyta została zarejestrowana.</param>
+        /// <param name="PatientFirstName">Imię pacjenta, dla którego dana wizyta została zarejestrowana.</param>
+        /// <param name="IsEmergency">Określa czy wskazana wizyta została zarejestrowana jako nagły przypadek.</param>
+        public DailyVisitsListItem(DateTime DateOfVisit, string PatientLastName, string PatientFirstName, bool IsEmergency)
+        {
+            this.DateOfVisit = DateOfVisit;
+            this.PatientLastName = PatientLastName;
+            this.PatientFirstName = PatientFirstName;
+            this.IsEmergency = IsEmergency;
+        }
+
+        #endregion // Ctors
+    }
+}
