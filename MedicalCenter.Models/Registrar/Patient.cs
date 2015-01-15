3e7b940559db78e350590a000463a60a97185d9a
AdamGraser <adamgraser@interia.pl>
2015-01-15 08:12:34 +0100

    Modyfikacja modelu Registrar\Patient
    Zmieniono przestrzeń nazw na właściwą dla rejestratorki.

--- MedicalCenter.Models/Registrar/Patient.cs
+++ MedicalCenter.Models/Registrar/Patient.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MedicalCenter.Models
+namespace MedicalCenter.Models.Registrar
 {
     /// <summary>
     /// Reprezentuje obiekt klasy M_Patient w widoku dla rejestratorki.
