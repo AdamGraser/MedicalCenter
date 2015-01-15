20e1775b11fdac2865a4ba8d0a4303704c624422
AdamGraser <adamgraser@interia.pl>
2015-01-15 02:03:07 +0100

    Dod. modelu Registrar\Month + zm. AbsencesList
    AbsencesList: usunięto implementację interfejsu IList, zmieniono
    prywatne pole 'doctorsList' w publiczną właściwość 'DoctorsList'.

---
+++ MedicalCenter.Models/Registrar/Month.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCenter.Models.Registrar
+{
+    /// <summary>
+    /// Reprezentuje zbiór list nieobecnych lekarzy ze wszystkich dni wskazanego miesiąca w danym roku.
+    /// </summary>
+    class Month : IList
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Lista lekarzy nieobecnych w danym dniu.
+        /// </summary>
+        private List<AbsencesList> absencesList;
+
+        #endregion // Private fields
+
+        #region Private properties
+
+        /// <summary>
+        /// Zwraca wartość określającą czy dostęp do kolekcji ICollection jest synchronizowany pomiędzy wątkami. (Odziedziczone z ICollection.)
+        /// Zwraca wartość false.
+        /// </summary>
+        private bool IsSynchronized
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Zwraca obiekt, który może zostać użyty do synchronizacji dostępu do kolekcji ICollection. (Odziedziczone z ICollection.)
+        /// Zwraca null.
+        /// </summary>
+        private object SyncRoot
+        {
+            get { return null; }
+        }
+
+        #endregion // Private properties
+
+        #region Public properties
+
+        /// <summary>
+        /// Miesiąc, z którego nieobecności przechowywane są w tej liście.
+        /// </summary>
+        public int Month { get; set; }
+
+        /// <summary>
+        /// Rok, z którego miesiąc jest brany pod uwagę.
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Umożliwia pobranie referencji do elementu listy o wskazanym indeksie.
+        /// Zmiana elementu o wskazanym indeksie jest dostępna tylko wewnątrz klasy.
+        /// </summary>
+        /// <param name="index">Indeks elementu, do którego referencja ma zostać zwrócona.</param>
+        /// <returns>Element o wskazanym indeksie lub null, jeśli podany indeks jest spoza zakresu.</returns>
+        public AbsencesList this[int index]
+        {
+            get
+            {
+                try
+                {
+                    return absencesList[index];
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+            private set
+            {
+                try
+                {
+                    absencesList[index] = value;
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Określa czy rozmiar listy jest stały.
+        /// </summary>
+        public bool IsFixedSize
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Określa czy lista jest tylko do odczytu.
+        /// </summary>
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Zwraca liczbę elementów w liście.
+        /// </summary>
+        public int Count
+        {
+            get { return absencesList.Count; }
+        }
+
+        #endregion // Public properties
+
+        #region Ctors
+
+        /// <summary>
+        /// Konstruktor tworzący pustą listę lekarzy.
+        /// </summary>
+        public Month()
+        {
+            absencesList = new List<AbsencesList>();
+        }
+
+        #endregion // Ctors
+
+        #region Public methods
+
+        /// <summary>
+        /// Dodaje nowy element na koniec listy.
+        /// </summary>
+        /// <param name="newItem">Element, który ma zostać dodany do listy.</param>
+        public void Add(AbsencesList newItem)
+        {
+            absencesList.Add(newItem);
+        }
+
+        /// <summary>
+        /// Usuwa wszystkie elementy z listy.
+        /// </summary>
+        public void Clear()
+        {
+            absencesList.Clear();
+        }
+
+        /// <summary>
+        /// Determinuje czy lista zawiera podany element.
+        /// </summary>
+        /// <param name="item">Element, którego obecność w liście ma zostać sprawdzona.</param>
+        /// <returns>Zwraca true jeśli lista zawiera podany element, false w przeciwnym razie.</returns>
+        public bool Contains(AbsencesList item)
+        {
+            return absencesList.Contains(item);
+        }
+
+        /// <summary>
+        /// Podaje indeks w liście wskazanego elementu.
+        /// </summary>
+        /// <param name="item">Element, którego indeks ma zostać podany.</param>
+        /// <returns>Zwraca indeks podanego elementu lub -1, jeśli nie znaleziono.</returns>
+        public int IndexOf(AbsencesList item)
+        {
+            return absencesList.IndexOf(item);
+        }
+
+        /// <summary>
+        /// Wstawia element do listy na pozycję wskazywaną przez podany indeks.
+        /// </summary>
+        /// <param name="index">Indeks, jaki ma mieć element po wstawieniu do listy.</param>
+        /// <param name="newItem">Element, który ma zostać wstawiony do listy.</param>
+        public void Insert(int index, AbsencesList newItem)
+        {
+            try
+            {
+                absencesList.Insert(index, newItem);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Usuwa z listy pierwsze znalezione wystąpienie podanego elementu.
+        /// </summary>
+        /// <param name="item">Element, którego pierwsze wystąpienie ma zostać usunięte z listy.</param>
+        public void Remove(AbsencesList item)
+        {
+            absencesList.Remove(item);
+        }
+
+        /// <summary>
+        /// Usuwa z listy element o podanym indeksie.
+        /// </summary>
+        /// <param name="index">Indeks elementu, który ma zostać usunięty.</param>
+        public void RemoveAt(int index)
+        {
+            try
+            {
+                absencesList.RemoveAt(index);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Kopiuje wszystkie elementy listy do podanej tablicy, wstawiając pierwszy element na pozycję o wskazanym indeksie.
+        /// </summary>
+        /// <param name="array">Tablica, do której mają zostać skopiowane elementy listy.</param>
+        /// <param name="index">Indeks, jaki ma mieć pierwszy element listy po skopiowaniu do tablicy.</param>
+        public void CopyTo(AbsencesList[] array, int index)
+        {
+            try
+            {
+                absencesList.CopyTo(array, index);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Zwraca enumerator tej listy.
+        /// </summary>
+        /// <returns>Zwraca enumerator tej listy.</returns>
+        public IEnumerator GetEnumerator()
+        {
+            return absencesList.GetEnumerator();
+        }
+
+        #endregion // Public methods
+    }
+}
