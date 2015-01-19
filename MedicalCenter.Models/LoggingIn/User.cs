47020f8dd7cf6ac5bff5c5d1fa1a6316175844c2
AdamGraser <adamgraser@interia.pl>
2015-01-20 00:15:53 +0100

    Dodanie modelu LoggingIn\User
    + inkrementacja wersji pliku i biblioteki
    + zaległa inkrementacja wersji pliku i biblioteki projektu GUI

---
+++ MedicalCenter.Models/LoggingIn/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCenter.Models.LoggingIn
+{
+    /// <summary>
+    /// Reprezentuje użytkownika systemu.
+    /// </summary>
+    class User
+    {
+        #region Public properties
+
+        /// <summary>
+        /// Przechowuje wprowadzoną w formularzu logowania nazwę użytkownika systemu.
+        /// Jest to wartość z kolumny Login z tabeli A_Users.
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Przechowuje hash wprowadzonego w formularzu logowania hasła dostępu do konta użytkownika.
+        /// Jest to wartość z kolumny Password z tabeli A_Users.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Przechowuje ID rekordu z tabeli A_Workers, reprezentującego osobę aktualnie zalogowaną.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Przechowuje nazwisko aktualnie zalogowanej osoby.
+        /// Jest to wartość z kolumny LastName z tabeli A_Workers.
+        /// </summary>
+        public string LastName { get; set; }
+        
+        /// <summary>
+        /// Przechowuje imię aktualnie zalogowanej osoby.
+        /// Jest to wartość z kolumny FirstName z tabeli A_Workers.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        #endregion // Public properties
+
+        #region Ctors
+
+        /// <summary>
+        /// Konstruktor tworzący obiekt z domyślną wartością Id.
+        /// </summary>
+        public User()
+        {
+            Id = 0;
+        }
+
+        #endregion // Ctors
+    }
+}
