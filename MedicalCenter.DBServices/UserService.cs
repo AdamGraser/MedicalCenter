13e209247f7001d469d148e3841d83f77350b452
AdamGraser <adamgraser@interia.pl>
2015-01-20 20:49:15 +0100

    Serwisy bazodanowy i biznesowy m.in. dla logowania
    - zmiana nazwy LogInService na UserService
    - utworzenie serwisu biznesowego UserBusinessService
    - zdefiniowanie metod, właściwości i części CRUD'a potrzebnej do
    przeprowadzenia logowania (brakuje tylko prezentera, odpowiednich
    właściwości i bindingów w widoku i jakichś zdarzeń, czy czegoś...)

---
+++ MedicalCenter.DBServices/UserService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedicalCenter.Models.LoggingIn;
+using MedicalCenter.Data;
+
+namespace MedicalCenter.DBServices
+{
+    /// <summary>
+    /// Obsługa podstawowych operacji na tabelach związanych z użytkownikami systemu (pracownikami).
+    /// </summary>
+    public class UserService
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Obiekt kontekstu bazodanowego, używany do wykonywania operacji na bazie danych.
+        /// </summary>
+        MedicalCenterDBContainer db;
+
+        #endregion // Private fields
+
+        #region Ctors
+
+        /// <summary>
+        /// Bezargumentowy konstruktor inicjalizujący obiekt kontekstu bazodanowego.
+        /// </summary>
+        public UserService()
+        {
+            db = new MedicalCenterDBContainer();
+        }
+
+        #endregion // Ctors
+
+        #region Public methods
+
+        /// <summary>
+        /// Pobiera z bazy danych informacje o użytkowniku identyfikowanym przez podany login i hash hasła.
+        /// </summary>
+        /// <param name="predicate">Funkcja (predykat) sprawdzająca warunek dla każdego elementu.</param>
+        /// <returns>
+        /// Obiekt reprezentujący rekord z tabeli A_Users, odpowiadający szukanemu użytkownikowi,
+        /// lub obiekt z wartościami domyślnymi, jeżeli nie znaleziono użytkownika o podanym loginie i/lub hashu hasła.
+        /// </returns>
+        public A_User SelectUser(System.Linq.Expressions.Expression<Func<A_User, bool>> predicate)
+        {
+            return db.A_Users.FirstOrDefault(predicate);
+        }
+
+        /// <summary>
+        /// Pobiera z bazy danych informacje o wskazanym pracowniku.
+        /// </summary>
+        /// <param name="predicate">Funkcja (predykat) sprawdzająca warunek dla każdego elementu.</param>
+        /// <returns>
+        /// Obiekt reprezentujący rekord z tabeli A_Workers,
+        /// lub obiekt z wartościami domyślnymi, jeżeli podane ID jest spoza zakresu.
+        /// </returns>
+        public A_Worker SelectWorker(System.Linq.Expressions.Expression<Func<A_Worker, bool>> predicate)
+        {
+            return db.A_Workers.FirstOrDefault(predicate);
+        }
+
+        /// <summary>
+        /// Pobiera z bazy danych informacje o wskazanym stanowisku służbowym.
+        /// </summary>
+        /// <param name="predicate">Funkcja (predykat) sprawdzająca warunek dla każdego elementu.</param>
+        /// <returns>
+        /// Obiekt reprezentujący rekord z tabeli A_DictionaryJobTitle,
+        /// lub obiekt z wartościami domyślnymi, jeżeli podane ID jest spoza zakresu.
+        /// </returns>
+        public A_DictionaryJobTitle SelectJobTitle(System.Linq.Expressions.Expression<Func<A_DictionaryJobTitle, bool>> predicate)
+        {
+            return db.A_DictionaryJobTitles.FirstOrDefault(predicate);
+        }
+
+        #endregion // Public methods
+    }
+}
---
+++ MedicalCenter.Services/UserBusinessService.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedicalCenter.DBServices;
+using MedicalCenter.Models.LoggingIn;
+using MedicalCenter.Data;
+
+namespace MedicalCenter.Services
+{
+    /// <summary>
+    /// Obsługa zaawansowanych scenariuszy logiki biznesowej aplikacji w zakresie użytkowników systemu (pracowników).
+    /// </summary>
+    public class UserBusinessService
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Przechowuje nazwisko aktualnie zalogowanej osoby.
+        /// Jest to wartość z kolumny LastName z tabeli A_Workers.
+        /// </summary>
+        string lastName;
+
+        /// <summary>
+        /// Przechowuje imię aktualnie zalogowanej osoby.
+        /// Jest to wartość z kolumny FirstName z tabeli A_Workers.
+        /// </summary>
+        string firstName;
+
+        /// <summary>
+        /// Przechowuje nazwę stanowiska aktualnie zalogowanej osoby.
+        /// Jest to wartość z kolumny JobTitle z tabeli A_DictionaryJobTitle.
+        /// </summary>
+        string jobTitle;
+
+        /// <summary>
+        /// Usługa bazodanowa dla funkcjonalności obejmującej użytkowników systemu.
+        /// </summary>
+        UserService userService;
+
+        #endregion // Private fields
+
+        #region Public properties
+
+        /// <summary>
+        /// Zwraca imię i nazwisko aktualnie zalogowanej osoby, oddzielone spacją.
+        /// </summary>
+        public string Title
+        {
+            get
+            {
+                return jobTitle + " - " + firstName + " " + lastName;
+            }
+
+            private set
+            {
+                jobTitle = firstName = lastName = "";
+            }
+        }
+
+        #endregion // Public properties
+
+        #region Ctors
+
+        /// <summary>
+        /// Konstruktor tworzący usługę bazodanową na potrzeby usług biznesowych obejmujących użytkowników systemu.
+        /// </summary>
+        public UserBusinessService()
+        {
+            userService = new UserService();
+        }
+
+        #endregion // Ctors
+
+        #region Public methods
+
+        /// <summary>
+        /// Sprawdza w bazie danych podane poświadczenia.
+        /// Jeśli znaleziono w bazie odpowiadającego użytkownika, pobierane są również jego imię, nazwisko i nazwa stanowiska.
+        /// </summary>
+        /// <param name="user">Obiekt zawierający podane login i hasło.</param>
+        /// <returns>ID osoby, do której przypisany jest wskazany użytkownik systemu lub 0, jeśli podane poświadczenia nie figurują w bazie danych.</returns>
+        public int LogIn(User user)
+        {
+            System.Security.Cryptography.HashAlgorithm sha = System.Security.Cryptography.HashAlgorithm.Create("SHA512");
+
+            // sprawdzenie czy w systemie istnieje użytkownik o podanym loginie i hashu hasła
+            A_User usr = userService.SelectUser(x => x.Login == user.Login &&
+                                              x.Password == System.Text.Encoding.ASCII.GetString(sha.ComputeHash(System.Text.Encoding.ASCII.GetBytes(user.Password))));
+            
+            // jeśli podane poświadczenia są prawidłowe
+            if (usr.WorkerId > 0)
+            {
+                // pobranie informacji o pracowniku, do którego przypisany jest sprawdzony użytkownik systemu
+                A_Worker wrk = userService.SelectWorker(x => x.Id == usr.WorkerId);
+
+                // jeśli rekord użytkownika w bazie zawiera prawidłowe ID pracownika
+                if (wrk.Id > 0)
+                {
+                    // zapisanie imienia i nazwiska pracownika
+                    lastName = wrk.LastName;
+                    firstName = wrk.FirstName;
+
+                    // pobranie informacji o stanowisku służbowym pracownika
+                    A_DictionaryJobTitle djt = userService.SelectJobTitle(x => x.Id == wrk.JobTitle);
+
+                    // jeśli rekord pracownika w bazie zawiera prawidłowe ID stanowiska
+                    if (djt.Id > 0)
+                    {
+                        // zapisanie nazwy stanowiska
+                        jobTitle = djt.JobTitle;
+                    }
+                }
+            }
+
+            return usr.WorkerId;
+        }
+
+        #endregion // Public methods
+    }
+}
