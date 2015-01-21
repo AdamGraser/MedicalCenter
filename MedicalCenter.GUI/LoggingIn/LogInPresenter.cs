2923c3d516c7ed2a760fac7fae1c59f2399c96e4
AdamGraser <adamgraser@interia.pl>
2015-01-21 07:48:15 +0100

    Dodanie prezentera LoggingIn/LogInPresenter
    + odpowiednie referencje dla projektu GUI

---
+++ MedicalCenter.GUI/LoggingIn/LogInPresenter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedicalCenter.Services;
+
+namespace MedicalCenter.GUI.LoggingIn
+{
+    /// <summary>
+    /// Obsługa zdarzeń użytkownika (działania na modelach i serwisach pod wpływem zdarzeń z widoku).
+    /// </summary>
+    public class LogInPresenter
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Logika biznesowa w zakresie użytkowników systemu.
+        /// </summary>
+        UserBusinessService userBusinessService;
+
+        #endregion // Private fields
+
+        #region Ctors
+
+        /// <summary>
+        /// Konstruktor tworzący obiekt logiki biznesowej w zakresie użytkowników systemu.
+        /// </summary>
+        public LogInPresenter()
+        {
+            userBusinessService = new UserBusinessService();
+        }
+
+        #endregion // Ctors
+    }
+}
