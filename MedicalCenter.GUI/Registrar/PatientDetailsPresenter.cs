e795ad18adb276ef68de31c69f0175c1459cf59c
AdamGraser <adamgraser@interia.pl>
2015-01-24 00:20:18 +0100

    Dopieszczanie szczegółów pacjenta cz. 1
    Prezenter:
    - dodano właściwość IsFormCompleted, określającą czy formularz został
    poprawnie wypełniony
    - dodano metodę PeselChanged, która wywoływana z handler'a zdarzenia
    zmiany tekstu w polu na pesel, sprawdza, czy przekazano tam wartość z
    obiektu PatientDetails i czy ma ona odpowiednią długość
    - dodano metodę ValidatePesel, która wywoływana z handler'a zdarzenia
    utraty focus'a klawiatury przez pole na pesel, sprawdza, czy wprowadzony
    pesel jest prawidłowy oraz czy cały formularz jest aktualnie poprawnie
    wypełniony, by w razie czego aktywować przycisk "Zapisz"

--- MedicalCenter.GUI/Registrar/PatientDetailsPresenter.cs
+++ MedicalCenter.GUI/Registrar/PatientDetailsPresenter.cs
@@ -18,7 +18,49 @@ namespace MedicalCenter.GUI.Registrar
         /// </summary>
         PatientDetailsView view;
 
+        /// <summary>
+        /// Określa czy podczas walidacji pól formularza znalezione zostały błędy.
+        /// </summary>
+        bool validationErrors;
+
+        /// <summary>
+        /// Instancja struktury Thickness, definiująca grubość linii o wartości 1.0, używana przy obramowaniach elementów formularza.
+        /// </summary>
+        System.Windows.Thickness Thickness1;
+
+        /// <summary>
+        /// Instancja struktury Thickness, definiująca grubość linii o wartości 2.0, używana przy obramowaniach elementów formularza.
+        /// </summary>
+        System.Windows.Thickness Thickness2;
+
         #endregion // Private fields
+
+        #region Private properties
+
+        /// <summary>
+        /// Określa czy formularz został poprawnie wypełniony.
+        /// </summary>
+        bool IsFormCompleted
+        {
+            get
+            {
+                if (!validationErrors &&
+                    view.LastName.Text.Length > 0 &&
+                    view.FirstName.Text.Length > 0 &&
+                    view.BirthDate.SelectedDate != null &&
+                    view.Pesel.Text.Length > 0 &&
+                    view.BuildingNumber.Text.Length > 0 &&
+                    view.PostalCode.Text.Length > 0 &&
+                    view.City.Text.Length > 0)
+
+                    return true;
+
+                else
+                    return false;
+            }
+        }
+
+        #endregion // Private properties
 
         #region Ctors
 
@@ -29,6 +71,9 @@ namespace MedicalCenter.GUI.Registrar
         public PatientDetailsPresenter(PatientDetailsView view)
         {
             this.view = view;
+            this.validationErrors = false;
+            Thickness1 = new System.Windows.Thickness(1.0);
+            Thickness2 = new System.Windows.Thickness(2.0);
         }
 
         #endregion // Ctors
@@ -54,7 +99,7 @@ namespace MedicalCenter.GUI.Registrar
                 view.FirstName.Clear();
                 view.SecondName.Clear();
                 view.BirthDate.SelectedDate = null;
-                view.Gender.SelectedIndex = -1;
+                view.Gender.SelectedIndex = 0;
                 view.Pesel.Clear();
                 view.Street.Clear();
                 view.BuildingNumber.Clear();
@@ -67,6 +112,168 @@ namespace MedicalCenter.GUI.Registrar
                 // powrót do menu głównego
                 view.ParentWindow.ContentArea.Content = view.ParentWindow.RegistrarMainMenuView;
             }
+        }
+
+        /// <summary>
+        /// Obsługa zdarzenia zmiany zawartości pola tekstowego "Pesel" w widoku szczegółów pacjenta.
+        /// </summary>
+        public void PeselChanged()
+        {
+            // jeśli pole nie jest edytowane, to wprowadzona wartość pochodzi ze zbindowanego obiektu "Patient PatientData"
+            if (!view.Pesel.IsKeyboardFocused)
+            {
+                // pesel to liczba - jeśli jest zerem, to pole na pesel ma być puste
+                if (view.Pesel.Text == "0")
+                    view.Pesel.Clear();
+                // pesel to liczba - brak zer z lewej strony
+                else if (view.Pesel.Text.Length == 10)
+                    view.Pesel.Text = "0" + view.Pesel.Text;
+            }
+        }
+
+        /// <summary>
+        /// Walidacja wartości wprowadzonej do pola "Nr PESEL" dla pól powiązanych: "Nr PESEL", "Data urodzenia", "Płeć".
+        /// </summary>
+        public void ValidatePesel()
+        {
+            // określa czy podany pesel jest nieprawidłowy
+            byte incorrectPesel = 0;
+            
+            // jeśli mniej niż 11 znaków, pesel jest nieprawidłowy
+            if (view.Pesel.Text.Length < 11)
+            {
+                incorrectPesel = 1;
+                view.Pesel.ToolTip = "Pesel musi mieć dokładnie 11 znaków!";
+            }
+            else
+            {
+                int temp;
+                
+                // jeśli podano datę urodzenia, można zwalidować pod tym kątem pesel
+                if (view.BirthDate.SelectedDate != null)
+                {
+                    // dwie pierwsze cyfry pesel'u to młodsza połowa roku
+                    int.TryParse(view.Pesel.Text.Substring(0, 2), out temp);
+
+                    // rok podany w dacie urodzenia musi mieścić się w przedziale <1800; 2299>
+                    // (o ile dobrze mi wiadomo, dla lat późniejszych, a tym bardziej wcześniejszych, nie zdefiniowano wielkości,
+                    // jakie miałyby być dodane do numerów miesięcy)
+                    if (view.BirthDate.SelectedDate.Value.Year < 1800 || view.BirthDate.SelectedDate.Value.Year > 2299)
+                        incorrectPesel = 2;
+                    else
+                    {
+                        // jeśli młodsza połowa numeru roku w podanej dacie urodzenia jest inna niż ta podana w peselu,
+                        // to należy zgłosić błąd w tych dwóch powiązanych ze sobą polach
+                        if (view.BirthDate.SelectedDate.Value.Year % 100 != temp)
+                            incorrectPesel = 2;
+                        else
+                        {
+                            // trzecia i czwarta cyfra pesel'u tworzą nr miesiąca
+                            int.TryParse(view.Pesel.Text.Substring(2, 2), out temp);
+                            
+                            // miesiąc z daty urodzenia inny niż podany w pesel'u -> błąd
+                            // (modulo 20, ponieważ https://pl.wikipedia.org/wiki/PESEL#Data_urodzenia)
+                            if (view.BirthDate.SelectedDate.Value.Month != temp % 20)
+                                incorrectPesel = 2;
+                            else
+                            {
+                                // piąta i szósta cyfra pesel'u tworzą dzień miesiąca
+                                int.TryParse(view.Pesel.Text.Substring(4, 2), out temp);
+                                
+                                // dzień z daty urodzenia inny niż podany w pesel'u -> błąd
+                                if (view.BirthDate.SelectedDate.Value.Day != temp)
+                                    incorrectPesel = 2;
+                            }
+                        }
+                    }
+                }
+
+                // walidacja pesel'u pod kątem płci
+                int.TryParse(view.Pesel.Text.Substring(9, 1), out temp);
+
+                // cyfra płci nieparzysta == mężczyzna -> jeśli wybrano kobietę == błąd
+                if ((temp & 1) == 1 && view.Gender.SelectedIndex == 1 || view.Gender.SelectedIndex == 0)
+                {
+                    // jeśli wcześniej wykryto błąd na linii pesel <-> data urodzenia
+                    if (incorrectPesel == 2)
+                        incorrectPesel = 4;
+                    else
+                        incorrectPesel = 3;
+                }
+
+                // sprawdzanie cyfry kontrolnej
+                double[] multipliers = { 1.0, 3.0, 7.0, 9.0, 1.0, 3.0, 7.0, 9.0, 1.0, 3.0, 1.0, 0.0 };
+
+                // walidacja cyfry kontrolnej sprowadza się do następującego działania:
+                // obliczenia wyrażenia a+3b+7c+9d+e+3f+7g+9h+i+3j+k (gdzie litery oznaczają kolejne cyfry numeru)
+                for (int i = 0; i < view.Pesel.Text.Length; ++i)
+                    multipliers[11] += char.GetNumericValue(view.Pesel.Text[i]) * multipliers[i];
+
+                // a następnie sprawdzenia czy reszta z dzielenia przez 10 jest zerem
+                if ((int)multipliers[11] % 10 != 0)
+                    incorrectPesel += 10;
+            }
+
+            // jeśli pesel jest nieprawidłowy, należy to zaznaczyć w formularzu
+            if (incorrectPesel > 0)
+            {
+                // ustawienie flagi błędów w formularzu
+                validationErrors = true;
+
+                // ustawienie czerwonych obramowań odpowiednim polom
+                view.Pesel.BorderBrush = System.Windows.Media.Brushes.Red;
+                view.Pesel.BorderThickness = Thickness2;
+
+                // komunikat o nieprawidłowej sumie kontrolnej numeru PESEL
+                string wrongChecksum = string.Empty;
+
+                // zgłoszenie tego faktu odbywa się poprzez dodanie do incorrectPesel wartości 10
+                if (incorrectPesel > 9)
+                {
+                    wrongChecksum = "Suma kontrolna numeru PESEL nie zgadza się!";
+                    incorrectPesel -= 10;
+                }
+
+                if (incorrectPesel == 0 && wrongChecksum.Length > 0)
+                    view.Pesel.ToolTip = wrongChecksum;
+                else
+                {
+                    if (wrongChecksum.Length > 0)
+                        wrongChecksum = "\n" + wrongChecksum;
+
+                    // jeśli błąd dotyczy także wartości dwóch pozostałych pól
+                    if (incorrectPesel == 4)
+                    {
+                        view.BirthDate.BorderBrush = view.Gender.BorderBrush = System.Windows.Media.Brushes.Red;
+                        view.BirthDate.BorderThickness = view.Gender.BorderThickness = Thickness2;
+                        view.BirthDate.ToolTip = view.Gender.ToolTip = view.Pesel.ToolTip = "Błąd w powiązanych polach Nr PESEL, Data urodzenia, Płeć!" + wrongChecksum;
+                    }
+                    // jeśli błąd dotyczy także wybranej płci
+                    else if (incorrectPesel == 3)
+                    {
+                        view.Gender.BorderBrush = System.Windows.Media.Brushes.Red;
+                        view.Gender.BorderThickness = Thickness2;
+                        view.Gender.ToolTip = view.Pesel.ToolTip = "Numer PESEL i płeć nie zgadzają się!" + wrongChecksum;
+                    }
+                    // jeśli błąd dotyczy także wybranej daty urodzenia
+                    else if (incorrectPesel == 2)
+                    {
+                        view.BirthDate.BorderBrush = System.Windows.Media.Brushes.Red;
+                        view.BirthDate.BorderThickness = Thickness2;
+                        view.BirthDate.ToolTip = view.Pesel.ToolTip = "Numer PESEL i data urodzenia nie zgadzają się!" + wrongChecksum;
+                    }
+                }
+            }
+            // pesel prawidłowy - przywracanie domyślnych ustawień pól formularza
+            else
+            {
+                view.Pesel.BorderThickness = view.BirthDate.BorderThickness = view.Gender.BorderThickness = Thickness1;
+                view.Pesel.BorderBrush = view.BirthDate.BorderBrush = view.Gender.BorderBrush = System.Windows.Media.Brushes.CornflowerBlue;
+                view.BirthDate.ToolTip = view.Gender.ToolTip = view.Pesel.ToolTip = null;
+            }
+
+            // jeśli formularz został poprawnie wypełniony, należy aktywować przycisk "Zapisz"
+            view.Save.IsEnabled = IsFormCompleted;
         }
 
         #endregion // Public methods
