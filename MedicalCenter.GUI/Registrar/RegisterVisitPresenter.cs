30c2662e2dac3d4f0b25eeb4b0436724c71319aa
AdamGraser <adamgraser@interia.pl>
2015-01-26 01:23:39 +0100

    Dodanie widoku i prezentera listy lekarzy
    Lista ta wyświetlana jest na pierwszym ekranie procedury rejestracji
    wizyty.

---
+++ MedicalCenter.GUI/Registrar/RegisterVisitPresenter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedicalCenter.Services;
+
+namespace MedicalCenter.GUI.Registrar
+{
+    /// <summary>
+    /// Obsługa zdarzeń użytkownika dla widoku szczegółów pacjenta (działania na modelach i serwisach pod wpływem zdarzeń z widoku).
+    /// </summary>
+    public class RegisterVisitPresenter
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Widok listy lekarzy (przy rejestracji wizyty) zarządzany przez tego prezentera.
+        /// </summary>
+        RegisterVisitView view;
+
+        #endregion // Private fields
+
+        #region Ctors
+
+        /// <summary>
+        /// Konstruktor zapisujący referencję do zarządzanego widoku i tworzący obiekty usług logiki biznesowej.
+        /// </summary>
+        /// <param name="view">Widok listy lekarzy (przy rejestracji wizyty) zarządzany przez tego prezentera.</param>
+        public RegisterVisitPresenter(RegisterVisitView view)
+        {
+            this.view = view;
+        }
+
+        #endregion // Ctors
+
+        #region Public methods
+
+        /// <summary>
+        /// Obsługa zdarzenia kliknięcia przycisku "Powrót" w widoku listy lekarzy przy rejestracji wizyty.
+        /// Czyści filtry, zaznaczenie i zmienia zawartość okna głównego aplikacji na menu główne.
+        /// </summary>
+        public void Back()
+        {
+            // wyczyszczenie filtrów
+            view.FilterClinicName.SelectedIndex = -1;
+            view.FilterDoctorName.Clear();
+
+            // wyczyszczenie zaznaczenia w tabeli
+            view.DoctorsListTable.SelectedIndex = -1;
+
+            // wyczyszczenie ID pacjenta
+            view.PatientId = 0;
+
+            // ustawienie dzisiejszej daty
+            view.TheDate.SelectedDate = DateTime.Today;
+
+            // przywrócenie widoku menu głównego
+            view.ParentWindow.ContentArea.Content = view.ParentWindow.History.Pop();
+        }
+
+        #endregion // Public methods
+    }
+}
---
+++ MedicalCenter.GUI/Registrar/RegisterVisitView.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MedicalCenter.Models.Registrar;
+
+namespace MedicalCenter.GUI.Registrar
+{
+    /// <summary>
+    /// Reprezentuje widok listy lekarzy przy rejestrowaniu nowej wizyty.
+    /// </summary>
+    public partial class RegisterVisitView : UserControl
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Prezenter obsługujący m.in. zdarzenia użytkownika.
+        /// </summary>
+        RegisterVisitPresenter registerVisitPresenter;
+
+        #endregion // Private fields
+
+        #region Public properties
+
+        /// <summary>
+        /// Przechowuje ID pacjenta, dla którego ma zostać zarejestrowana wizyta (0 jeśli jeszcze nie wybrano).
+        /// </summary>
+        public int PatientId;
+
+        /// <summary>
+        /// Lista obiektów zawierających nazwę poradni, nazwisko i imię lekarza, nr gabinetu oraz liczbę pacjentów, których lekarz przyjąć ma w danym dniu.
+        /// </summary>
+        public List<DoctorsListItem> DoctorsList;
+
+        /// <summary>
+        /// Lista nazw poradni i ID odpowiadających im rekordów z tabeli M_DictionaryClinics.
+        /// </summary>
+        public Dictionary<int, string> ClinicsList;
+
+        /// <summary>
+        /// Filtr poradni.
+        /// </summary>
+        public ComboBox FilterClinicName;
+
+        /// <summary>
+        /// Filtr lekarzy.
+        /// </summary>
+        public TextBox FilterDoctorName;
+
+        /// <summary>
+        /// Okno główne, którego treść stanowi ten widok.
+        /// </summary>
+        public MainWindow ParentWindow;
+
+        #endregion // Public properties
+
+        #region Ctors
+
+        /// <summary>
+        /// Konstruktor inicjalizujący pola i właściwości.
+        /// </summary>
+        /// <param name="parentWindow">Okno główne, którego treść stanowi ten widok.</param>
+        public RegisterVisitView(MainWindow parentWindow)
+        {
+            InitializeComponent();
+
+            // zapisanie referencji do nadrzędnego okna
+            this.ParentWindow = parentWindow;
+
+            // utworzenie prezentera
+            registerVisitPresenter = new RegisterVisitPresenter(this);
+
+            // zapisanie domyślnego ID pacjenta
+            PatientId = 0;
+
+            // utworzenie listy lekarzy (poradnia, lekarz, liczba pacjentów w wybranym dniu, nr gabinetu)
+            DoctorsList = new List<DoctorsListItem>();
+
+            // utworzenie listy poradni medycznych
+            ClinicsList = new Dictionary<int, string>();
+
+            // ustawienie kontekstu danych tabeli, aby móc z nią powiązać listy lekarzy i poradni
+            DoctorsListTable.DataContext = this;
+
+            // zapisanie referencji do filtru poradni
+            StackPanel header = DoctorsListTable.Columns[0].Header as StackPanel;
+            FilterClinicName = header.Children.OfType<ComboBox>().First(x => x.Name == "FilterClinicName");
+
+            // zapisanie referencji do filtru lekarzy
+            header = DoctorsListTable.Columns[1].Header as StackPanel;
+            FilterDoctorName = header.Children.OfType<TextBox>().First(x => x.Name == "FilterDoctorName");
+        }
+
+        #endregion // Ctors
+
+        #region Events handlers
+
+        /// <summary>
+        /// Obsługa zdarzenia kliknięcia przycisku "Powrót".
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            // wyczyszczenie filtrów, zaznaczenia, przywrócenie domyślnej daty i powrót do menu głównego
+            registerVisitPresenter.Back();
+        }
+
+        #endregion // Events handlers
+    }
+}
